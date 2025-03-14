2a3f306334658ac4580cc910dbe99008a170caa7
Muhammad Anies Wahdie <manies5396@gmail.com>
2025-03-14 14:05:50 +0700

    fix: export image from assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,25 @@
 import LogoPleasureLove from './logos/logo.png';
+import ProductAPDS from './products/APDS/292ec182-de49-4504-9681-e60f9a500041.jpg';
+import ProductAPFP from './products/APFP/3dd46d8f-c584-4375-9cdb-4960e1973ae8.jpg';
+import ProductAVST from './products/AVST/015b3385-d2bb-4071-af47-eba6c23cce29.jpg';
+import ProductBHB from './products/BHB/11f3d123-9ea9-4d9a-9346-f294930d4dde.jpg';
+import ProductBRN from './products/BRN/137c6b90-981b-410f-8343-e0d5c894341f.jpg';
+import ProductBSBB from './products/BSBB/4d178f99-3385-47cc-8ddd-a1d022b52a7e.jpg';
+import ProductBTAS from './products/BTAS/032e154e-ac48-4018-bdf0-36aeff58d52b.jpg';
+import ProductCLJV from './products/CLJV/39061db4-8dd0-4d98-891d-7de72435306b.jpg';
+import ProductCSVOG from './products/CSVOG/0599a822-78a5-4dc4-98f0-5e9ebe3cb104.jpg';
+import ProductVibratorWireless from './products/vibrator-wanita-wireless/3940379b-7dc1-4990-8a9b-a48b5fe7d474.jpg';
 
 export {
-  LogoPleasureLove
+  ProductVibratorWireless,
+  ProductCSVOG,
+  ProductCLJV,
+  ProductBTAS,
+  ProductBSBB,
+  ProductBRN,
+  ProductBHB,
+  ProductAVST,
+  ProductAPFP,
+  ProductAPDS,
+  LogoPleasureLove,
 }
