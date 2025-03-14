cfa2e82f2f0926c3be473873dab467991530b6a7
Muhammad Anies Wahdie <manies5396@gmail.com>
2025-03-14 08:46:03 +0700

    fix: add product images

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Image } from "antd";
 import React from "react";
 
 const Main = () => {
@@ -9,10 +9,30 @@ const Main = () => {
         flexDirection: "column",
         width: "100%",
         padding: "5px",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <div>Coba Yah</div>
-      <Button type="primary">Primary</Button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "300px",
+        }}
+      >
+        <div>Coba Yah</div>
+        <div>
+          <Image
+            alt="gambar-produk"
+            src={
+              "../assets/products/vibrator-wanita-wireless/9ba6ca66-df23-4df7-86da-f2c45ad00d9d.jpg"
+            }
+            width={500}
+            height={500}
+          />
+        </div>
+        <Button type="primary">Primary</Button>
+      </div>
     </div>
   );
 };
